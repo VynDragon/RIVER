6ae0cabda7834097ef8ab84af61554e08cbb28a9
VynDragon <Vdragon.b@gmail.com>
2016-08-06 19:23:27 +0200

    izehfiuza

---
+++ include/cpu.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include ISA.h
+
+#define HARTNUMBER		1
+
+typedef struct Hart { // no core structure as they are seen as harts by the code running.
+	WORD	registers[32];
+	WORD	pc;
+} Hart;
+
+typedef struct CPU {
+	Hart harts[HARTNUMBER];
+	
+} CPU;
---
+++ src/main.c
@@ -0,0 +1,6 @@
+#include "ISA.h"
+
+int main(int ac, char **av)
+{
+	
+}
